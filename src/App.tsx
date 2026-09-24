c68b360f85359b1691b9eaaf6ac5d3c8f5b48644
dymovicd123 <dymovicd@gmail.com>
2026-09-24 18:38:26 +0500

    CLIENT-ZAMMLER-F2: remove stale Workshop selection binding

--- src/App.tsx
+++ src/App.tsx
@@ -954,7 +954,6 @@ function App() {
     selectedInventoryOperationGroup,
     selectedInventoryOperationItems,
     selectedOrder,
-    selectedWorkshopTasks,
     summary,
     updateInventoryArrivalPosition,
     updateInventoryArrivalSize,
